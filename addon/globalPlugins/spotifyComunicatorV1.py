993229030936e934f361385a15f7a365b8c670fe
Derrick Day <brlkid@outlook.com>
2022-11-01 07:58:58 -0400

    first commit

---
+++ addon/globalPlugins/spotifyComunicatorV1.py
@@ -0,0 +1,20 @@
+import globalPluginHandler
+from scriptHandler import script
+import ui
+from api import getDesktopObject
+
+spotify = None 
+
+if spotify==None:
+    for child in getDesktopObject().children:
+        if "spotify" in str(child.appModule).lower():
+            spotify = child
+
+class GlobalPlugin(globalPluginHandler.GlobalPlugin):
+    @script(gesture="kb:NVDA+t", description = "Reports the title of the currently playing Spotify song if the Spotify app is running")
+    def script_getSongTitle(self, gesture):
+        if spotify==None or spotify.name==None: 
+            ui.message("Spotify linking failed. Either start the Spotify app, or press ctrl+NVDA+f3 to reload linking.")
+        else: 
+            ui.message(spotify.name)
+
---
+++ readme.md
@@ -0,0 +1,12 @@
+# Spotify Communicator #
+
+This package contains a basic integration for the Windows Spotify application. 
+At current, it can read the title of the currently playing track however functionality to copy metadata to the clipboard is coming. 
+
+This package is distributed under the terms of the GNU General Public License, version 2 or later. Please see the file COPYING.txt for further details.
+
+commands: 
+NVDA+t: This will read the name of the currently playing Spotify track. 
+
+note: the addon will inform the user if the Spotify app is not detected. If this is the case, one must start the Spotify app, then either press NVDA+CTRL+f3 or restart NVDA to allow the addon to reload the detection of Spotify. 
+
